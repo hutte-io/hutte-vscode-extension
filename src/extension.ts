34d1368428aed54d07f78cb2e2ec7bade695a39d
Manuel Moya Ferrer <mmoyaferrer@gmail.com>
2023-05-31 11:44:42 +0200

    draft of 0.1 version

--- src/extension.ts
+++ src/extension.ts
@@ -12,30 +12,57 @@ export function activate(context: vscode.ExtensionContext) {
 			const email = await vscode.window.showInputBox({title: 'Hutte Email Address'});
 			const password = await vscode.window.showInputBox({title: 'Hutte Password', password: true});
 
+			const vscodeOutput : vscode.OutputChannel = vscode.window.createOutputChannel('Hutte');
+			vscodeOutput.show();
+
 			try {
 				const output = await commandSync(`sfdx hutte:auth:login --email ${email} --password ${password}`);
 				if (output.stdout.includes('Hutte: Invalid Login Credentials')) {
 					vscode.window.showErrorMessage(output.stdout);
+					vscodeOutput.appendLine(output.stdout);
 				} else {
 					vscode.window.showInformationMessage('Hutte: Successfully Authorized');
+					vscodeOutput.appendLine('Hutte: Successfully Authorized');
 				}
 			} catch (err: any) {
 				vscode.window.showErrorMessage(err.message);
+				vscodeOutput.appendLine(err.message);
 			}
 		})
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('hutte.getOrgFromPool', async () => {
-			let output;
+			const vscodeOutput : vscode.OutputChannel = vscode.window.createOutputChannel('Hutte');
+			vscodeOutput.show();
 
+			let output;
 			try {
 				output = await commandSync(`sfdx hutte:pool:take --wait --json`);
 				vscode.window.showInformationMessage('Hutte: Successfully Set Org from Pool');
+				vscodeOutput.appendLine('Hutte: Successfully Set Org from Pool');
 			} catch(err: any) {
 				vscode.window.showErrorMessage(err.message);
+				vscodeOutput.appendLine(err.message);
 			}
-			
+
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('hutte.getOrg', async () => {
+			const vscodeOutput : vscode.OutputChannel = vscode.window.createOutputChannel('Hutte');
+			vscodeOutput.show();
+
+			let output;
+			try {
+				output = await commandSync(`echo | sfdx hutte:org:authorize`);
+				vscode.window.showInformationMessage('Hutte: Successfully Set Org');
+				vscodeOutput.appendLine('Hutte: Successfully Set Org');
+			} catch(err: any) {
+				vscode.window.showErrorMessage(err.message);
+				vscodeOutput.appendLine(err.message);
+			}
 		})
 	);
 }
