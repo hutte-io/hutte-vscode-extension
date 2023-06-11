26ea7ba0d2f9ed1ed35332f2045163b2eceaac5e
Manuel Moya Ferrer <mmoyaferrer@gmail.com>
2023-06-11 14:01:20 +0200

    Remove sample code

--- src/extension.ts
+++ src/extension.ts
@@ -64,24 +64,6 @@ export function activate(context: vscode.ExtensionContext) {
 			authorizeOrg('');
 		})
 	);
-
-	const myCommandId = 'sample.showSelectionCount';
-	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, (n) => {
-		vscode.window.showInformationMessage(`Hutte Sample, ${n} line(s) selected... Keep going!`);
-	}));
-
-	// create a new status bar item that we can now manage
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	myStatusBarItem.command = myCommandId;
-	context.subscriptions.push(myStatusBarItem);
-
-	// register some listener that make sure the status bar 
-	// item always up-to-date
-	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
-
-	// update status bar item once at start
-	updateStatusBarItem();
 }
 
 // This method is called when your extension is deactivated
@@ -133,20 +115,3 @@ async function activateFromPool() {
 	});
 }
 
-function updateStatusBarItem(): void {
-	const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-	if (n > 0) {
-		myStatusBarItem.text = `$(megaphone) Hutte Sample, ${n} line(s) selected`;
-		myStatusBarItem.show();
-	} else {
-		myStatusBarItem.hide();
-	}
-}
-
-function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
-	let lines = 0;
-	if (editor) {
-		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
-	}
-	return lines;
-}
