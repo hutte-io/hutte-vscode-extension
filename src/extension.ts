dbd2594d1b47b55b85dc7b184f11d3aa3c8a21fc
Manuel Moya Ferrer <mmoyaferrer@gmail.com>
2023-06-11 14:30:07 +0200

    Handling authenticationg

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-// import { DepNodeProvider, Dependency } from './nodeDependencies';
 import { HutteOrgsProvider, HutteOrg } from './hutteOrgsProvider';
-import { commandSync,  } from "execa";
+import { commandSync  } from "execa";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	try {
+		commandSync(`sfdx hutte:org:list --json --includeauth`);
+		vscode.commands.executeCommand('setContext', 'hutte.IsLogged', true);
+	} catch(err){
+		vscode.commands.executeCommand('setContext', 'hutte.IsLogged', false);
+	}
+
 	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
 		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
@@ -34,19 +41,35 @@ export function activate(context: vscode.ExtensionContext) {
 			const vscodeOutput : vscode.OutputChannel = vscode.window.createOutputChannel('Hutte');
 			vscodeOutput.show();
 
-			try {
-				const output = await commandSync(`sfdx hutte:auth:login --email ${email} --password ${password}`);
-				if (output.stdout.includes('Hutte: Invalid Login Credentials')) {
-					vscode.window.showErrorMessage(output.stdout);
-					vscodeOutput.appendLine(output.stdout);
-				} else {
-					vscode.window.showInformationMessage('Hutte: Successfully Authorized');
-					vscodeOutput.appendLine('Hutte: Successfully Authorized');
+
+			await vscode.window.withProgress({
+				location: vscode.ProgressLocation.Notification,
+				title: "Authenticating into Hutte",
+				cancellable: false
+			}, (progress, token) => {
+				progress.report({ message: 'Setting Hutte Org' });
+				
+				try {
+					const output = commandSync(`sfdx hutte:auth:login --email ${email} --password ${password}`);
+					if (output.stdout.includes('Hutte: Invalid Login Credentials')) {
+						progress.report({message: output.stdout});
+						vscodeOutput.appendLine(output.stdout);
+						vscode.commands.executeCommand('setContext', 'hutte.IsLogged', false);
+
+						return Promise.reject();
+					} else {
+						progress.report({message: 'Hutte: Successfully Authorized'});
+						vscodeOutput.appendLine('Hutte: Successfully Authorized');
+						vscode.commands.executeCommand('setContext', 'hutte.IsLogged', true);
+
+						return Promise.resolve();
+					}
+				} catch (err: any) {
+					vscode.window.showErrorMessage(err.message);
+					vscodeOutput.appendLine(err.message);
+					return Promise.reject();
 				}
-			} catch (err: any) {
-				vscode.window.showErrorMessage(err.message);
-				vscodeOutput.appendLine(err.message);
-			}
+			});
 		})
 	);
 
@@ -73,7 +96,7 @@ async function authorizeOrg(orgName?: string) {
 
 	await vscode.window.withProgress({
 		location: vscode.ProgressLocation.Notification,
-		title: "Progress Notification",
+		title: "Authorizing Hutte Org",
 		cancellable: false
 	}, (progress, token) => {
 		progress.report({ message: 'Setting Hutte Org' });
@@ -87,7 +110,6 @@ async function authorizeOrg(orgName?: string) {
 
 		return Promise.resolve();
 	});
-	
 }
 
 async function activateFromPool() {
@@ -96,14 +118,14 @@ async function activateFromPool() {
 
 	await vscode.window.withProgress({
 		location: vscode.ProgressLocation.Notification,
-		title: "Progress Notification",
+		title: "Activating Org From Pool",
 		cancellable: false
 	}, (progress, token) => {
 		progress.report({ message: 'Taking Hutte Org from Pool' });
 		try {
 			commandSync(`sfdx hutte:pool:take --wait --json`);
-			vscode.window.showInformationMessage('Hutte: Successfully Set Org from Pool');
-			vscodeOutput.appendLine('Hutte: Successfully Set Org from Pool');
+			vscode.window.showInformationMessage('Hutte: Successfully Activated Org from Pool');
+			vscodeOutput.appendLine('Hutte: Successfully Activated Org from Pool');
 		} catch(err: any) {
 			vscode.window.showErrorMessage(err.message);
 			vscodeOutput.appendLine(err.message);
@@ -112,4 +134,3 @@ async function activateFromPool() {
 		return Promise.resolve();
 	});
 }
-
